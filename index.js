8a84e4b579df980bcfefcdee9d8469d2c9670f54
Hunter Kreshock <hkreshock@gmail.com>
2024-05-29 21:38:19 -0700

    cleanups

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function ClientAccessAPI() {
     const getCollectionObjects = async (collectionID, clientId, clientSecret) => {
         const { getCollectionObjectsCall, getClientTokenCall } = serviceCalls();
         const { serveCookie } = cookieFns();
-        
+
         let token = null;
         const tokenObj = serveCookie("token");
         if (tokenObj) token = JSON.parse(tokenObj);
 
         if (!token) {
-            return await getClientTokenCall(clientId, clientSecret);
+            token = await getClientTokenCall(clientId, clientSecret);
         }
         return await getCollectionObjectsCall(collectionID, token);
     };
