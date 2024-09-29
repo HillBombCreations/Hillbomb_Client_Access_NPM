60782f7c8c2d1126063a012676016de8ab27c8ad
Justin Ceccarelli <justinceccarelli@gmail.com>
2024-09-28 17:43:48 -0700

    Changed to collection ID

--- serviceCalls.js
+++ serviceCalls.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 function serviceCalls() {
-    const getCollectionObjectsCall = async (groupName, key, contentType) => {
+    const getCollectionObjectsCall = async (groupName, key, collectionId) => {
         const { data } = await axios.get(
             "https://client.vivreal.io/tenant/collectionObjects",
             {
-                params: { groupName, key, contentType },
+                params: { groupName, key, collectionId },
             }
         );
         return data.map((obj) => obj.objectValue);
